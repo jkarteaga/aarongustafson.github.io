2d3dbc5b9330135c342055a69c721211a064bc32
Aaron Gustafson <aaron@easy-designs.net>
2016-10-11 13:49:43 -0400

    Site updated at 2016-10-11 17:49:36 UTC

--- j/post.js
+++ j/post.js
@@ -34,29 +34,10 @@
 (function( window, document ) {
     'use strict';
 
-    var $discuss_script = document.createElement('script'),
-    	script_name = '//' + disqus_shortname + '.disqus.com/' + disqus_script;
-   	
-	if ( 'AG' in window &&
-		 'preconnect' in window.AG )
-	{
-        window.AG.preconnect( '//disqus.com/' );
-        window.AG.preconnect( '//' + disqus_shortname + '.disqus.com' );
-        window.AG.preconnect( '//links.services.disqus.com/' );
-        window.AG.preconnect( '//a.disquscdn.com' );
-        window.AG.prefetch( script_name );
-        if ( script_name.indexOf('count') < 0 )
-        {
-            window.AG.prefetch( '//' + disqus_shortname + '.disqus.com/count.js' );
-        }
-    }
-	
-	$discuss_script.type = 'text/javascript';
-    $discuss_script.async = true;
-    $discuss_script.src = script_name;
-    
-    (document.head || document.body).appendChild( $discuss_script );
-
+    var $s = document.createElement('script');
+    $s.src = '//' + disqus_config.AG.shortname + '.disqus.com/' + disqus_config.AG.script;
+    $s.setAttribute( 'data-timestamp', '' + new Date() );
+    (document.head || document.body).appendChild( $s );
     $discuss_script = null;
 
 }( this, this.document ));
