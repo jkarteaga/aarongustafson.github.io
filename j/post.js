21fe6121ff55e2c9a2bc7e65815f5206593b96eb
Aaron Gustafson <aaron@easy-designs.net>
2017-01-11 15:17:10 -0500

    Site updated at 2017-01-11 20:16:33 UTC

--- j/post.js
+++ j/post.js
@@ -213,6 +213,207 @@
     }
 
 }( this ));
+if ( 'speechSynthesis' in window )
+{
+	// Housekeeping
+	window.addEventListener( 'beforeunload', function(){
+		window.speechSynthesis.cancel();
+	});
+
+	function play() {
+		// don’t run twice
+		if ( playing ) { return; }
+		playing = true;
+
+		if ( speaking )
+		{
+			console.log( 'resume', speech );
+			window.speechSynthesis.resume();
+		}
+		else
+		{
+			speaking = true;
+			console.log( 'play', speech );
+			window.speechSynthesis.speak( speech );
+		}
+		
+		// reset the button
+		setTimeout(toggleButtons, 500);
+	}
+
+	function pause() {
+		// don’t run twice
+		if ( pausing ) { return; }
+		pausing = true;
+
+		window.speechSynthesis.pause();
+		console.log( 'pause', speech );
+
+		setTimeout(toggleButtons, 500);
+	}
+
+	function adjustSpeed()
+	{
+		// cancel the original utterance
+		console.log('cancelling the original utterance');
+		window.speechSynthesis.cancel();
+
+		// find the previous space
+		console.log( 'progress char', progress_index );
+		var previous_space = speech.text.lastIndexOf( ' ', progress_index );
+		console.log( 'found a space at char', previous_space );
+		
+		// get the remains of the original string
+		speech.text = speech.text.slice( previous_space );
+		console.log( 'new text', speech.text );
+
+		// Math to 1 decimal place
+		speed = Math.round( $speed_value.value * 10 ) / 10;
+
+		// adjust the rate
+		if ( speed > 10 )
+		{
+			speed = 10;
+			$faster.disabled = true;
+		}
+		else if ( speed < 0.1 )
+		{
+			speed = 0.1;
+			$slower.disabled = true;
+		}
+		speech.rate = speed;
+
+		// return to speaking
+		console.log( 'starting a new one', speech );
+		window.speechSynthesis.speak( speech );
+	}
+
+	function toggleButtons() {
+		playing = false;
+		pausing = false;
+		if ( $play.disabled === false )
+		{
+			//$play.disabled = true;
+			//$pause.disabled = false;
+			//$speed_value.disabled = false;
+		}
+		else
+		{
+			//$play.disabled = false;
+			//$pause.disabled = true;
+			//$speed_value.disabled = true;
+		}
+	}
+
+	var speech = new SpeechSynthesisUtterance(),
+		speaking = false,
+		pausing = false,
+		playing = false,
+		text,
+		progress_index = 0,
+		$content = document.querySelector('main').cloneNode(true),
+		$space = $content.querySelectorAll('pre'),
+		$pause_before = $content.querySelectorAll('h2, h3, h4, h5, h6, p, li, dt, blockquote, pre, figure, footer'),
+		$skip = $content.querySelectorAll('.anchorable__anchor, aside, .dont_read'),
+		$intro = document.createElement('dt'),
+		$controls = document.createElement('dd'),
+		$button = document.createElement('button'),
+		$buttons = document.createElement('p'),
+		$play,
+		$pause,
+		default_speed = 1.4,
+		$speed = $buttons.cloneNode(true),
+		$speed_label = document.createElement('label'),
+		$speed_value = document.createElement('input'),
+		$caveat = $buttons.cloneNode(true);
+	
+	// Don’t read
+	$skip.forEach(function( $el ){
+		$el.innerHTML = '';
+	});
+
+	// spacing out content
+	$space.forEach(function($el){
+		$el.innerHTML = ' ' + $el.innerHTML.replace(/[\r\n\t]/g, ' ') + ' ';
+	});
+
+	// Synthetic Pauses
+	$pause_before.forEach(function( $el ){
+		$el.innerHTML = ' , ' + $el.innerHTML;
+	});
+
+	// capture character index as we move.
+	speech.onboundary = function( e ) {
+		if ( e.name == 'word' )
+		{
+			progress_index = e.charIndex;
+		}
+	};
+
+	// clear the queue when done
+	speech.onend = function( e ) {
+		window.speechSynthesis.cancel();
+	};
+
+	text = $content.textContent;
+	speech.text = text;
+	
+	//----------------------
+	// Build the GUI
+	// ---------------------
+	
+	$intro.innerHTML = 'Would you prefer to have this post read to you?';
+	
+	$controls.classList.add('media-controls');
+	
+	// Buttons
+	$buttons.classList.add('media-controls__buttons');
+	$button.classList.add('media-controls__button');
+	// Default active buttons
+	$play = $button.cloneNode('true');
+	$play.classList.add('media-controls__button--play');
+	$play.setAttribute( 'aria-label', 'Play' );
+	$play.addEventListener( 'click', play, false );
+	$play.addEventListener( 'touchend', play, false );
+	$buttons.appendChild($play);
+	// Default disabled buttons
+	//$button.disabled = true;
+	$pause = $button.cloneNode('true');
+	$pause.classList.add('media-controls__button--pause');
+	$pause.setAttribute( 'aria-label', 'Pause' );
+	$pause.addEventListener( 'click', pause, false );
+	$pause.addEventListener( 'touchend', pause, false );
+	$buttons.appendChild($pause);
+	$controls.appendChild($buttons);
+
+	// Speed
+	//$speed.classList.add('media-controls__speed');
+	speech.rate = default_speed;
+	//$speed_label.innerText = 'Speed';
+	//$speed_label.htmlFor = 'speed_value';
+	//$speed.appendChild( $speed_label );
+	//$speed_value.type = 'number';
+	//$speed_value.id = 'speed_value';
+	//$speed_value.disabled = true;
+	//$speed_value.min = '0.1';
+	//$speed_value.max = '10';
+	//$speed_value.step = '0.1';
+	//$speed_value.value = default_speed;
+	//$speed.appendChild( $speed_value );
+	//$speed.addEventListener( 'change', adjustSpeed, false );
+	//$speed.addEventListener( 'blur', adjustSpeed, false );
+	//$speed.addEventListener( 'keyup', adjustSpeed, false );
+	//$speed.addEventListener( 'click', adjustSpeed, false );
+	//$speed.addEventListener( 'touchend', adjustSpeed, false );
+	//$controls.appendChild($speed);
+
+	// Caveat
+	$caveat.innerHTML = '<small>SpeechSynthesis is still experimental. This could be buggy</small>'
+	$controls.appendChild($caveat);
+
+	document.querySelector('.entry__meta').appendChild($intro);
+	document.querySelector('.entry__meta').appendChild($controls);
+}
 /**
  *  WebMentions.io JS
  *  A re-tooling of Aaron Parecki’s recommended JS for using the WebMention.io API
--- j/serviceworker.js
+++ j/serviceworker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var version = 'v1484084900373:',
+var version = 'v1484164745422:',
 	default_avatar = 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y',
 	missing_image = 'https://i.imgur.com/oWLuFAa.gif';
 self.addEventListener( 'activate', function( event ){
